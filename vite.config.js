57b9e23386ae75e626b08dd95f46116aced5b4cf
Abisu <99135ddd@gmail.com>
2026-02-14 23:46:56 +0800

    feat: 更新手機版面

--- vite.config.js
+++ vite.config.js
@@ -16,13 +16,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
+            src: 'favicon-ios.png',
+            sizes: '76x76',
             type: 'image/png'
           }
         ]
